7b1f7cb54c4141a63d5949bf8ec5793d03a4b6d6
John Hartnett <john.hartnett@1account.net>
2023-07-25 17:35:03 +0100

    add images from old app

--- web/frontend/assets/index.js
+++ web/frontend/assets/index.js
@@ -1,2 +1,19 @@
-export { default as notFoundImage } from "./empty-state.svg";
-export { default as trophyImage } from "./home-trophy.png";
+export { default as notFoundImage } from './empty-state.svg'
+export { default as trophyImage } from './home-trophy.png'
+export { default as avCloseFailTag } from './av-close-and-fail-tag.png'
+export { default as avCloseTag } from './av-close-tag.png'
+export { default as avFailTag } from './av-fail-tag.png'
+export { default as avPendingTag } from './av-pending-tag.png'
+export { default as avTag } from './av-tag.png'
+export { default as changeProject } from './change-project.png'
+export { default as codeToPaste } from './code-to-paste.png'
+export { default as howToPasteCode } from './how-to-paste-code.png'
+export { default as howToPasteCode2 } from './how-to-paste-code2.png'
+export { default as mainScreenFilled } from './main-screen-filled.png'
+export { default as mainScreenFilled1 } from './main-screen-filled1.png'
+export { default as mainScreen } from './main-screen.png'
+export { default as mainScreen1 } from './main-screen1.png'
+export { default as myProjects } from './my-projects.png'
+
+
+
