3642784e7a4dcdcc5d1f8e13de3f1c79e99bd56d
John Hartnett <john.hartnett@1account.net>
2023-07-27 10:15:51 +0100

    is Productino condition allowing easy dev startup

--- web/shopify.js
+++ web/shopify.js
@@ -1,20 +1,21 @@
 import { LATEST_API_VERSION } from '@shopify/shopify-api'
 import { shopifyApp } from '@shopify/shopify-app-express'
-// import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from '@shopify/shopify-api/rest/admin/2023-04'
 
-// const DB_PATH = `${process.cwd()}/database.sqlite`;
+const ONE_ACCOUNT_SHOPIFY_APP_URL = process.env.ONE_ACCOUNT_SHOPIFY_APP_URL
+const isProduction = process.env.NODE_ENV === 'production'
 
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
     restResources,
     billing: undefined, // or replace with billingConfig above to enable example billing
-    apiKey: process.env.SHOPIFY_API_KEY,
-    apiSecretKey: process.env.SHOPIFY_API_SECRET,
-    // scopes: ['your_scopes'],
-    // hostScheme: 'http', dependant on node env
-    hostName: 'test-shopify.1accountqa.net',
+    ...(isProduction && {
+      apiKey: process.env.SHOPIFY_API_KEY,
+      apiSecretKey: process.env.SHOPIFY_API_SECRET,
+      hostScheme: 'https',
+      hostName: ONE_ACCOUNT_SHOPIFY_APP_URL,
+    }),
   },
   auth: {
     path: '/api/auth',
@@ -23,8 +24,6 @@ const shopify = shopifyApp({
   webhooks: {
     path: '/api/webhooks',
   },
-  // This should be replaced with your preferred storage strategy
-  // sessionStorage: new SQLiteSessionStorage(DB_PATH),
 })
 
 export default shopify
